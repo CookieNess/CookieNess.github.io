7aec0c32702528b7af1b2d8b432f098f5809258a
CookieNess <frederikkjaergaard@yahoo.dk>
2022-06-20 00:07:46 +0200

    Fixed multiple line generation on terminal

--- js/start_overlay.js
+++ js/start_overlay.js
@@ -1,43 +1,71 @@
 let i = -1;
+let j = 0;
 const terminalTextID = "#terminal_text";
 let terminalText = '';
+const userAgent = navigator.userAgent;
 
 $(document).ready(function () {
     const docHeight = $(document).height();
-    $("body").append("<div id='terminal_overlay'><p id='terminal_text'>&gt;</p><span class='terminal_cursor'>&#32; </span></div>");
     $(terminalTextID).height(docHeight);
     createTerminalText();
+    $("body").append("<div id='terminal_overlay'><p id='terminal_text'>&gt; </p><span class='terminal_cursor'>&#32; </span></div>");
     typeText();
-    //$("#terminal_text").append("<p>Test</p>")
-    //console.log(navigator.userAgent)
 });
 
 function createTerminalText() {
-    let userAgent = navigator.userAgent;
     let browser = '';
-    if (userAgent.match(/chrome|chromium|crios/i)) {
+    let browserVersion = '';
+    const chromeReg = /chrome|chromium|crios/i;
+    const safariReg = /safari/i;
+    const edgeReg = /edg/i;
+    const firefoxReg = /firefox|fxios/i;
+    const operaReg = /opr\//i;
+    if (userAgent.match(chromeReg)) {
         browser = "Google Chrome";
-        browserVersion = userAgent.match(/chrome|chromium|crios/i);
-    } else if (userAgent.match(/safari/i)) {
+        browserVersion = getBrowserVersion(chromeReg);
+    } else if (userAgent.match(safariReg)) {
         browser = "Safari Apple";
-    } else if (userAgent.match(/edg/i)) {
+        browserVersion = getBrowserVersion(safariReg);
+    } else if (userAgent.match(edgeReg)) {
         browser = "Microsoft Edge";
-    } else if (userAgent.match(/firefox|fxios/i)) {
+        browserVersion = getBrowserVersion(edgeReg);
+    } else if (userAgent.match(firefoxReg)) {
         browser = "Mozilla Firefox";
-    } else if (userAgent.match(/opr\//i)) {
-        browser = "opera";
+        browserVersion = getBrowserVersion(firefoxReg);
+    } else if (userAgent.match(operaReg)) {
+        browser = "Opera";
+        browserVersion = getBrowserVersion(operaReg);
     } else {
-        browser = "No browser detection";
+        browser = "What Browser Is This?";
+        browserVersion = "Version What?"
     }
+    terminalText = browser + ' [Version ' + browserVersion + ']';
+
+}
+
+function getBrowserVersion(browserReg) {
+    return (userAgent.split((userAgent.match(browserReg) + '/'))[1]).split(' ')[0];
 }
 
 function typeText() {
-    if (i < text.length) {
+    if (i < terminalText.length) {
         setTimeout(function () {
-            $(terminalTextID).get(0).innerHTML += text.charAt(i) + "<span class='terminal_cursor'>&#32;</span>";
+            $(terminalTextID).get(0).innerHTML += terminalText.charAt(i) + "<span class='terminal_cursor'>&#32;</span>";
         }, 5);
         $(".terminal_cursor").remove();
         i++;
         setTimeout(typeText, 50);
+    } else {
+        setTimeout(function(){$(terminalTextID).append("<br>&gt; ")}, 400);
+        setTimeout(function() {
+            if (j < 2) {
+                i = -1;
+                console.log(j);
+                j++;
+                setTimeout(typeText(), 400);
+            }
+        }, 400);
+
+        
     }
 }
