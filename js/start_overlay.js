4dbf858898bede7eca3176fdd45a62f09bd64115
CookieNess <frederikkjaergaard@yahoo.dk>
2022-06-19 14:53:14 +0200

    Overlay text + browser detection
    Overlay text generating done.
    Can detect browser, but not version yet.

--- js/start_overlay.js
+++ js/start_overlay.js
@@ -1,21 +1,43 @@
-var i = 0;
-var txt = 'Hello World';
-var speed = 50;
+let i = -1;
+const terminalTextID = "#terminal_text";
+let terminalText = '';
 
-$(document).ready(function() {
-    var docHeight = $(document).height();
-
-   $("body").append("<div id='overlay'><p id='text'>&gt;</p><span class='blink_cursor'>&#32; </span></div>");
-   $("#overlay").height(docHeight);
-      typeText();
+$(document).ready(function () {
+    const docHeight = $(document).height();
+    $("body").append("<div id='terminal_overlay'><p id='terminal_text'>&gt;</p><span class='terminal_cursor'>&#32; </span></div>");
+    $(terminalTextID).height(docHeight);
+    createTerminalText();
+    typeText();
+    //$("#terminal_text").append("<p>Test</p>")
     //console.log(navigator.userAgent)
 });
 
+function createTerminalText() {
+    let userAgent = navigator.userAgent;
+    let browser = '';
+    if (userAgent.match(/chrome|chromium|crios/i)) {
+        browser = "Google Chrome";
+        browserVersion = userAgent.match(/chrome|chromium|crios/i);
+    } else if (userAgent.match(/safari/i)) {
+        browser = "Safari Apple";
+    } else if (userAgent.match(/edg/i)) {
+        browser = "Microsoft Edge";
+    } else if (userAgent.match(/firefox|fxios/i)) {
+        browser = "Mozilla Firefox";
+    } else if (userAgent.match(/opr\//i)) {
+        browser = "opera";
+    } else {
+        browser = "No browser detection";
+    }
+}
+
 function typeText() {
-    if (i < txt.length) {
-        $("#text").get(0).innerHTML += txt.charAt(i) + "<span class='blink_cursor'>&#32;</span>";
-        $(".blink_cursor").remove();
+    if (i < text.length) {
+        setTimeout(function () {
+            $(terminalTextID).get(0).innerHTML += text.charAt(i) + "<span class='terminal_cursor'>&#32;</span>";
+        }, 5);
+        $(".terminal_cursor").remove();
         i++;
-        setTimeout(typeText, speed);
+        setTimeout(typeText, 50);
     }
 }
