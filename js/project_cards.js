269778b475f3a59775038d80cab62cda1092bdb9
CookieNess <frederikkjaergaard@yahoo.dk>
2022-10-25 10:17:44 +0200

    Started modal work and accent highligting

--- js/project_cards.js
+++ js/project_cards.js
@@ -1,5 +1,28 @@
 const projectConID = "projectsCon";
 
 $(document).ready(function() {
-
 });
+
+function openCardBSG() {
+    var modal = document.getElementById("borsenModal");
+
+    // Get the button that opens the modal
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close_modal_btn")[0];
+
+    // When the user clicks on the button, open the moda
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
